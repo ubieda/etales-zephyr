7776ad284f453f3f69fa7e434a075d977aa36de2
Luis Ubieda <luisf@croxel.com>
2024-04-12 18:15:10 -0400

    tests: tdd: test_motion_enabled_polls_accel_once_per_sec: ok
    Signed-off-by: Luis Ubieda <luisf@croxel.com>

--- subsys/motion_detection/motion_detection.c
+++ subsys/motion_detection/motion_detection.c
@@ -1,8 +1,32 @@
 #include <zephyr/kernel.h>
 #include <motion_detection.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/device.h>
 
 bool motion_detect_enabled;
 motion_st_changed_t module_listener;
+
+const struct device *accel_dev = DEVICE_DT_GET(DT_NODELABEL(motion_accel));
+
+static void motion_detection_thread_fn(void *v1, void *v2, void *v3)
+{
+	while (true) {
+		if (motion_detect_enabled) {
+			struct sensor_value accel_val[3] = {0};
+
+			k_sleep(K_SECONDS(1));
+
+			(void)sensor_sample_fetch(accel_dev);
+			(void)sensor_channel_get(accel_dev, SENSOR_CHAN_ACCEL_XYZ, accel_val);
+		} else {
+			k_sleep(K_FOREVER);
+		}
+	}
+}
+
+K_THREAD_DEFINE(motion_detection_thread_id, CONFIG_MOTION_DETECTION_THREAD_STACK_SIZE,
+	motion_detection_thread_fn, NULL, NULL, NULL,
+	CONFIG_MOTION_DETECTION_THREAD_PRIO, 0, 0);
 
 int motion_detection_init(void)
 {
@@ -12,6 +36,8 @@ int motion_detection_init(void)
 	motion_detect_enabled = true;
 
 	module_listener(MOTION_ST_UNKNOWN);
+
+	k_thread_resume(motion_detection_thread_id);
 
 	return 0;
 }
